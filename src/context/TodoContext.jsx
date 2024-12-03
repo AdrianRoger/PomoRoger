8c842263bfc5ed40141874378cbcabdc24f9f522
Adrian Roger <adrianroge@gmail.com>
2024-12-03 20:55:07 -0300

    fix: modify time property to include short and long break

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -4,32 +4,44 @@ export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
   const [toDoList, setToDoList] = useState([]);
-  const [toDoTimer, setToDoTimer] = useState(50);
+  const [toDoTime, setToDoTime] = useState({
+    time: 50,
+    shortBreak: 10,
+    longBreak: 25,
+  });
   const [editingTask, setEditingTask] = useState(null);
   const [taskToDelete, setTaskToDelete] = useState(null);
+  const [selectedTask, setSelectedTask] = useState(null);
 
   const getFromLocalStorage = (key) => {
     const storedData = localStorage.getItem(key);
     try {
       return storedData ? JSON.parse(storedData) : null;
     } catch (error) {
-      console.error(`Erro ao analisar JSON do localStorage para a chave "${key}":`, error);
+      console.error(
+        `Erro ao analisar JSON do localStorage para a chave "${key}":`,
+        error
+      );
       return null;
     }
-  }
+  };
 
   const setToLocalStorage = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  const handlerToDoTimer = (min) => {
-    setToDoTimer(min);
-    setToLocalStorage('timer', min);
   };
 
-  const addTask = ({ taskName, taskDt, pomodoroQtd }) => {
+  const handlerToDoTime = (time) => {
+    setToDoTime(time);
+    setToLocalStorage("time", time);
+  };
+
+  const handleSelectedTask = (task) => {
+    setSelectedTask(task);
+  };
+
+  const addTask = ({ taskName, taskDt, pomodoroQtd, status = false }) => {
     const newId = toDoList.length ? toDoList[toDoList.length - 1].id + 1 : 1;
-    const newtask = { id: newId, taskName, taskDt, pomodoroQtd };
+    const newtask = { id: newId, taskName, taskDt, pomodoroQtd, status };
     setToDoList((prev) => [...prev, newtask]);
   };
 
@@ -45,26 +57,26 @@ export const TodoProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const timer = getFromLocalStorage('timer');
-    if(!timer || isNaN(timer) || timer < 1){
-      setToLocalStorage('timer', toDoTimer);
-      return
+    const time = getFromLocalStorage("time");
+    if (!time || isNaN(time) || time < 1) {
+      setToLocalStorage("time", toDoTime);
+      return;
     }
 
-    setToDoTimer(Number(timer));
+    setToDoTime(Number(time));
   }, []);
 
   useEffect(() => {
-    const storedToDoList = getFromLocalStorage('tasks');
-    if(!storedToDoList || storedToDoList.length === 0){
-      setToLocalStorage('tasks', toDoList);
-    }else{
+    const storedToDoList = getFromLocalStorage("tasks");
+    if (!storedToDoList || storedToDoList.length === 0) {
+      setToLocalStorage("tasks", toDoList);
+    } else {
       setToDoList(storedToDoList);
     }
   }, []);
 
   useEffect(() => {
-    setToLocalStorage('tasks', toDoList);
+    setToLocalStorage("tasks", toDoList);
   }, [toDoList]);
 
   return (
@@ -74,12 +86,14 @@ export const TodoProvider = ({ children }) => {
         addTask,
         updateTask,
         deleteTask,
-        toDoTimer,
-        handlerToDoTimer,
+        toDoTime,
+        handlerToDoTime,
         editingTask,
         setEditingTask,
         taskToDelete,
-        setTaskToDelete
+        setTaskToDelete,
+        selectedTask,
+        handleSelectedTask,
       }}
     >
       {children}
