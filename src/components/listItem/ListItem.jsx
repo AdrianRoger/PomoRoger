2e0a2b8ebcf2676aee18c33db0d6d8d615e5dbfc
Adrian Roger <adrianroge@gmail.com>
2025-01-01 17:01:47 -0300

    feat: create select task logic

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,5 +1,5 @@
 import "./ListItem.css";
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { TodoContext } from "../../context/TodoContext";
 import AccessAlarmRoundedIcon from "@mui/icons-material/AccessAlarmRounded";
 import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
@@ -10,16 +10,26 @@ const ListItem = ({ data, handleEdit }) => {
   const iconSize = { fontSize: "11px" };
   const actionIconSize = { fontSize: "16px"};
 
-  const { setTaskToDelete, updateTask } = useContext(TodoContext);
+  const { setTaskToDelete, updateTask, selectedTask, handleSelectedTask } =
+    useContext(TodoContext);
 
   const handleChange = (e) => {
     updateTask({ ...data, status: e.target.checked });
   };
-  
+
+  const toggleSelectedTask = (e) => {
+    const ignoredTagNames = ["INPUT", "svg", "path"];
+
+    if (ignoredTagNames.some((tag) => e.target.tagName === tag)) return;
+    if (data.id === selectedTask?.id) return;
+
+    handleSelectedTask(data);
+  };
+
   return (
     <>
-      <div className="item">
-        <div className="overlay">
+      <div className={data.id === selectedTask?.id ? "item selected" : "item"}>
+        <div className="overlay" onClick={(e) => toggleSelectedTask(e)}>
           <input type="checkbox" checked={data.status} onChange={handleChange}></input>
           <div className="task">
             <dt className={data.status ? "task-name done" : "task-name"}>{data.taskName}</dt>
@@ -34,7 +44,11 @@ const ListItem = ({ data, handleEdit }) => {
               </span>
             </dd>
           </div>
-          <EditRoundedIcon className="color-btn action-btn" sx={actionIconSize} onClick={() => handleEdit(data)}/>
+          <EditRoundedIcon
+            className="color-btn action-btn"
+            sx={actionIconSize}
+            onClick={() => handleEdit(data)}
+          />
           <DeleteForeverRoundedIcon
             className="color-btn action-btn"
             sx={actionIconSize}
